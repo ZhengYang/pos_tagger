e6cc0c5d9302198a545c04d360592ca999ff13c4
Zheng Yang <zhengyang4k@gmail.com>
2012-10-13 16:35:47 +0800

    builder cleanup and taglist/wordlist

--- build_tagger.java
+++ build_tagger.java
@@ -1,7 +1,13 @@
-import java.io.*;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Arrays;
 
 class build_tagger {
 	public static void main(String[] args) {
@@ -16,16 +22,17 @@ class build_tagger {
         String modelFile = args[2];
         
         // init in-memory stats variables
+        Set<String> tagSet = new HashSet<String>();
+        Set<String> wordSet = new HashSet<String>();
         Map<String, Map<String, Integer>> TMatrix = new HashMap<String, Map<String, Integer>>();
         Map<String, Map<String, Integer>> EMatrix = new HashMap<String, Map<String, Integer>>();
         
         // train
         try {
-            FileInputStream fstream = new FileInputStream(trainFile);
-            DataInputStream in = new DataInputStream(fstream);
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            FileReader trainReader = new FileReader(trainFile);
+            BufferedReader trainBr = new BufferedReader(trainReader);
             String currLine;
-            while ((currLine = br.readLine()) != null) {
+            while ((currLine = trainBr.readLine()) != null) {
                 // break each line into an array of "word/tage" tokens
                 String[] tokens = currLine.trim().split("\\s+");
                 // keep track of the previous tag for transition matrix
@@ -42,6 +49,10 @@ class build_tagger {
                             word += "/" + wordAndTag[j];
                         }
                     }
+                    // cumulate the sets where applicable
+                    tagSet.add(tag);
+                    wordSet.add(word);
+                    
                     // cumulate emission count
                     if (EMatrix.containsKey(tag)) {
                         if (EMatrix.get(tag).containsKey(word)) {
@@ -96,23 +107,43 @@ class build_tagger {
                         
                 }
             }
-            in.close();
-        } catch (Exception e) {
-            System.err.println("Error: " + e.getMessage());
-        }
-        
-        // smoothing
-        
-        // output in-memory stats to <model_file>
-        try {
-            FileWriter fostream = new FileWriter(modelFile);
-            BufferedWriter out = new BufferedWriter(fostream);
-            for (Entry<String, Map<String, Integer>> entry : EMatrix.entrySet())
-            {
-                out.write(entry.getKey() + "/" + entry.getValue());
-            }
+            trainBr.close();
             
-            out.close();
+            // output in-memory stats to <model_file>
+            // arrange tag and word lists
+            String[] tagArray = tagSet.toArray(new String[0]);
+            String[] wordArray = wordSet.toArray(new String[0]);
+            Arrays.sort(tagArray);
+            Arrays.sort(wordArray);
+            
+            FileWriter modelWriter = new FileWriter(modelFile);
+            BufferedWriter modelBw = new BufferedWriter(modelWriter);
+            // 1st line of model file is tag list
+            String tagLine = "";
+            for (int i = 0; i < tagArray.length; i++)
+            {
+                tagLine += tagArray[i] + " ";
+            }
+            modelBw.write(tagLine.trim());
+            modelBw.newLine();
+            
+            // 2nd line of model file is word list
+            String wordLine = "";
+            for (int i = 0; i < wordArray.length; i++)
+            {
+                wordLine += wordArray[i] + " ";
+            }
+            modelBw.write(wordLine);
+            modelBw.newLine();
+            
+            /*
+            for (Entry<String, Map<String, Integer>> entry : EMatrix.entrySet())
+                        {
+                            out.write(entry.getKey() + "/" + entry.getValue());
+                        }
+            */
+            
+            modelBw.close();
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
         }
